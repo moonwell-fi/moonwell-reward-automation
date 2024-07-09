564c95ffb5a8158a7cde9ee465aae53b8d184894
Luke Youngblood <luke@moonwell.fi>
2024-07-08 17:46:51 -0700

    Added initial configs.

---
+++ src/config.ts
@@ -0,0 +1,35 @@
+export const comptrollers = [
+  {
+    address: "0xfBb21d0380beE3312B33c4353c8936a0F13EF26C",
+    chainId: 8453,
+    name: "Base",
+  },
+  {
+    address: "0x8E00D5e02E65A19337Cdba98bbA9F84d4186a180",
+    chainId: 1284,
+    name: "Moonbeam",
+  },
+]
+
+export const excludedMarkets = [
+  {
+    address: "0x02e9081DfadD37A852F9a73C4d7d69e615E61334",
+    chainId: 1284,
+    name: "mUSDC.mad",
+  },
+  {
+    address: "0xc3090f41Eb54A7f18587FD6651d4D3ab477b07a4",
+    chainId: 1284,
+    name: "mETH.mad",
+  },
+  {
+    address: "0x24A9d8f1f350d59cB0368D3d52A77dB29c833D1D",
+    chainId: 1284,
+    name: "mBTC.mad",
+  },
+  {
+    address: "0x298f2E346b82D69a473BF25f329BDF869e17dEc8",
+    chainId: 1284,
+    name: "mBUSD.wh",
+  },
+]
